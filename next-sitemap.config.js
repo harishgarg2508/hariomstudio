e8cbaf1d723040a69353546b721a2e7cb1c4bd04
Harish Garg <rishusharma123321@gmail.com>
2025-01-06 22:17:51 +0530

    sitemap build

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.hariomstudiobilaspur.in',
+    generateRobotsTxt: true,
+    generateIndexSitemap: true,
+    sitemapSize: 7000
+  }
