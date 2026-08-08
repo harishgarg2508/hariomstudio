6fb4c4f9cbe34edbafb6a055834658b11dbc0528
Harish Garg <rishusharma123321@gmail.com>
2026-08-08 09:47:53 +0530

    chore: update lint script to target all files in project directory and adjust next.config.mjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,10 @@ const nextConfig = {
   
   /* config options here */
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true, // Disables ESLint checks during builds
-  },
   typescript: {
     ignoreBuildErrors: true, // Ignores TypeScript errors during builds
   },
   reactStrictMode: false, // Disables React strict mode to prevent warnings
-  webpack: (config) => {
-    // Optional: Add custom webpack configuration if needed
-    return config;
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
